f16d404fbcff970b73988fa5081244cb7b9daa28
Qateef Ahmad <qateef.ahmad@educative.io>
2025-07-21 21:34:10 +0500

    changes

--- api/start_db.sh
+++ api/start_db.sh
@@ -1,19 +1,34 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-CONTAINER_NAME="devapp-mongodb"
-DATABASE_NAME="devapp"
+# CONTAINER_NAME="devapp-mongodb"
+# DATABASE_NAME="devapp"
+
+# docker container stop $CONTAINER_NAME
+# docker container rm $CONTAINER_NAME
+
+# mkdir -p .vol-data.mongo
+
+# docker run -d \
+#   --rm -it \
+#   --name $CONTAINER_NAME \
+#   -v ./.vol-data.mongo:/data/db \
+#   -p 27017:27017 \
+#   -e MONGO_INITDB_ROOT_USERNAME=user \
+#   -e MONGO_INITDB_ROOT_PASSWORD=pass \
+#   -e MONGO_INITDB_DATABASE=$DATABASE_NAME \
+#   mongo:6.0
+
+CONTAINER_NAME=dev_pg
 
 docker container stop $CONTAINER_NAME
 docker container rm $CONTAINER_NAME
 
-mkdir -p .vol-data.mongo
-
 docker run -d \
   --rm -it \
   --name $CONTAINER_NAME \
-  -v ./.vol-data.mongo:/data/db \
-  -p 27017:27017 \
-  -e MONGO_INITDB_ROOT_USERNAME=user \
-  -e MONGO_INITDB_ROOT_PASSWORD=pass \
-  -e MONGO_INITDB_DATABASE=$DATABASE_NAME \
-  mongo:6.0
+  -v ./postgres-data:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  -e POSTGRES_USER=user1 \
+  -e POSTGRES_PASSWORD=user1 \
+  -e POSTGRES_DB=user1 \
+  postgres:latest
